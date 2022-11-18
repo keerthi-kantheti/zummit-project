d2cd3a2442412380bb89594d4b66713417071a35
keerthi-kantheti <118546620+keerthi-kantheti@users.noreply.github.com>
2022-11-18 20:25:31 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,9 @@
+let formEl1=document.getElementById("personalDetailsForm");
+let formEl2=document.getElementById("yourReviewForm");
+let formEl3=document.getElementById("moreDetailsForm");
+let formEl4=document.getElementById("collegeLifeForm");
+let personalDetailsEl=document.getElementById("personalDetails");
+let yourReviewEl=document.getElementById("yourReview");
+let collegeLifeEl=document.getElementById("collegeLife");
+let moreDetailsEl=document.getElementById("moreDetails");
+console.log(moreDetailsEl)
